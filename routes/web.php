4b40634f711ac938615c3b59715f6daa29147a2a
d.soloviov <soloviovqqq@gmail.com>
2021-11-26 03:31:40 +0100

    add new route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,28 @@
 
 /** @var Router $router */
 
+use Faker\Factory;
+use Illuminate\Support\Str;
 use Laravel\Lumen\Routing\Router;
 
 $router->get('options', ['uses' => 'OptionController@getOptions']);
 
 $router->get('reload', function () {
     return response()->json(['reloadProtocol' => config('main.reload_protocol')]);
+});
+
+
+$router->get('generate-user', function () {
+    $faker = Factory::create('ru_RU');
+    $lastName = $faker->lastName;
+    $gender = substr($lastName, -1) === 'a' ? 'female' : 'male';
+    $firstName = $faker->firstName($gender);
+    $user = [
+        'first_name' => Str::ucfirst(Str::slug($firstName)),
+        'last_name' => Str::ucfirst(Str::slug($lastName)),
+        'gender' => $gender,
+        'username' => Str::slug($faker->word . $lastName),
+    ];
+
+    return response()->json($user);
 });
